bde0b2ce95005185c42216e9595d235b256381ab
ecodame <142754855+ecodame@users.noreply.github.com>
2023-10-10 18:48:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+function show(){
+    document.querySelector('.btn').classList.toggle('open')
+}
+function show2(){
+    document.querySelector('.btn2').classList.toggle('open2')
+}
+function show3(){
+    document.querySelector('.btn3').classList.toggle('open3')
+}
+function show4(){
+    document.querySelector('.btn4').classList.toggle('open4')
+}
+function show5(){
+    document.querySelector('.btn5').classList.toggle('open5')
+}
+function show6(){
+    document.querySelector('.btn6').classList.toggle('open6')
+}
+function show7(){
+    document.querySelector('.btn7').classList.toggle('open7')
+}
+function show8(){
+    document.querySelector('.btn8').classList.toggle('open8')
+}
+function show9(){
+    document.querySelector('.btn9').classList.toggle('open9')
+}
+function show10(){
+    document.querySelector('.btn10').classList.toggle('open10')
+}
+function show11(){
+    document.querySelector('.btn11').classList.toggle('open11')
+}
+
+function recup(){
+    document.querySelector('.skd').classList.toggle('aff')
+}
---
+++ goooo/op.js
@@ -0,0 +1,21 @@
+function opemn(){
+    document.querySelector('.coo').classList.toggle('add')
+}
+function opemn2(){
+    document.querySelector('.coo2').classList.toggle('add2')
+}
+function opemn3(){
+    document.querySelector('.coo3').classList.toggle('add3')
+}
+function opemn4(){
+    document.querySelector('.coo4').classList.toggle('add4')
+}
+function opemn5(){
+    document.querySelector('.coo5').classList.toggle('add5')
+}
+function opemn6(){
+    document.querySelector('.coo6').classList.toggle('add6')
+}
+function opemn7(){
+    document.querySelector('.coo7').classList.toggle('add7')
+}
